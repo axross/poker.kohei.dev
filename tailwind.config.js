626e9fecef0ac892339403cd833cef27849864d9
Kohei Asai <yo@kohei.dev>
2023-03-17 23:41:08 -0700

    feat: use next/font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./components/**/*.{ts,tsx}",
-  ],
+  content: ["./components/**/*.{ts,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        'card-symbol': ['"Azeret Mono"', 'ui-monospace'],
-      }
+        "card-symbol": ["var(--font-azeret-mono)", "ui-monospace"],
+      },
     },
   },
   plugins: [],
   darkMode: "class",
-}
+};
